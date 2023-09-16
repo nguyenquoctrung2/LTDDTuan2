af3dcb889c94679783ca1414d7ef571d8ef53355
nguyenquoctrung2 <nguyenquoctrung020525@gmail.com>
2023-09-17 01:08:57 +0700

    XMLHttpRequest

---
+++ 20032381_NguyenQuocTrung/slide11.js
@@ -0,0 +1,12 @@
+var url = "https://retoolapi.dev/PSi31i/data";
+        var xhr = new XMLHttpRequest();
+        xhr.open("DELETE", url + '/13', true);
+        xhr.onload = function () {
+            var users = JSON.parse(xhr.responseText);
+            if (xhr.readyState == 4 && xhr.status == "200") {
+                console.table(users);
+            } else {
+                console.error(users);
+            }
+        }
+        xhr.send(null);
